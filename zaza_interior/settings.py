7c1a05765c14e6c87014c20937bb4771e5230c11
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-09-14 16:44:33 +0300

    Update

--- zaza_interior/settings.py
+++ zaza_interior/settings.py
@@ -1,27 +1,15 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-i=x$!a9fgni8m=od#u_(-2q!az&!dp2%tb@$axj9a0qtop(+jb'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
 INSTALLED_APPS = [
-    # "djangocms_admin_style",
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -29,14 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'zaza_interior.web.apps.WebConfig',
-    
-    "django.contrib.sites",
-    "cms",
-    "menus",
-    "treebeard",
-    "sekizai",
-    "djangocms_versioning",
-    "djangocms_alias",
+
     "filer",
     "easy_thumbnails",
     "zaza_interior.gallery.apps.GalleryConfig"
@@ -52,11 +33,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     "django.middleware.locale.LocaleMiddleware",
-    "cms.middleware.user.CurrentUserMiddleware",
-    "cms.middleware.page.CurrentPageMiddleware",
-    "cms.middleware.toolbar.ToolbarMiddleware",
-    "cms.middleware.language.LanguageCookieMiddleware",
-    "cms.middleware.utils.ApphookReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'zaza_interior.urls'
@@ -75,8 +51,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
                 "django.template.context_processors.i18n",
-                "sekizai.context_processors.sekizai",
-                "cms.context_processors.cms_settings",
             ],
         },
     },
@@ -84,20 +58,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'zaza_interior.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,9 +80,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
 LANGUAGES = [
     ("en", "English"),
     ("bg", "Bulgarian"),
@@ -130,10 +93,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
@@ -146,18 +105,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Django CMS configs
-SITE_ID = 1
-CMS_CONFIRM_VERSION4 = True
-X_FRAME_OPTIONS = "SAMEORIGIN"
-# CMS_TEMPLATES = [
-#     ('web/index.html', 'Home page template'),
-# ]
 
 # Django filer configs
 THUMBNAIL_HIGH_RESOLUTION = True
--- zaza_interior/urls.py
+++ zaza_interior/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("zaza_interior.web.urls")),
-    path("cms/", include("cms.urls")),
     path("gallery/", include("zaza_interior.gallery.urls")),
 ]
 
