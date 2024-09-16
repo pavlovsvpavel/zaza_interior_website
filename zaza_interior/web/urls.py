4e81bc4344ce7cc8d7623fb09f6f3a4bc37fce92
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-09-16 14:19:21 +0300

    Added partial UI design

--- zaza_interior/web/urls.py
+++ zaza_interior/web/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from zaza_interior.web.views import IndexView
+from zaza_interior.web.views import IndexView, ContactsView, AboutView, HowItWorksView
 
 urlpatterns = (
     path("", IndexView.as_view(), name="index"),
+    path("how-it-works/", HowItWorksView.as_view(), name="how-it-works"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("about/", AboutView.as_view(), name="about"),
 )
--- zaza_interior/web/views.py
+++ zaza_interior/web/views.py
@@ -3,3 +3,13 @@ from django.views import generic as views
 class IndexView(views.TemplateView):
     template_name = "web/index.html"
 
+class HowItWorksView(views.TemplateView):
+    template_name = "web/how-it-works.html"
+
+
+class ContactsView(views.TemplateView):
+    template_name = "web/contacts.html"
+
+class AboutView(views.TemplateView):
+    template_name = "web/about.html"
+
