6e755e518c63655b7bb3581cd074f42bc80ed905
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-12-10 21:43:05 +0200

    Update

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Migrating database..."
+python manage.py migrate
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Creating locale files..."
+python manage.py makemessages -l bg
+python manage.py compilemessages
 
 echo "Starting Gunicorn and Nginx..."
 gunicorn --bind 0.0.0.0:10000 --access-logfile - --error-logfile - zaza_interior.wsgi:application & nginx -g 'daemon off;'
