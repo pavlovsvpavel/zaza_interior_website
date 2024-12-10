c24e0ccceb3790af5322a1360e27819a6a07713f
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-12-10 21:36:26 +0200

    Update

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn and Nginx..."
-gunicorn --bind 0.0.0.0:10000 --access-logfile - --error-logfile - zaza_interior.wsgi & nginx -g 'daemon off;'
+gunicorn --bind 0.0.0.0:10000 --access-logfile - --error-logfile - zaza_interior.wsgi:application & nginx -g 'daemon off;'
 
 
 
