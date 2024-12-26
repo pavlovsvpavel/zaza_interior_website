3aa453d2c837f61fde3670885570716d5b7e80d4
Pavel Pavlov <pavelsvpavlov@gmail.com>
2024-12-26 18:33:19 +0200

    Added BG translations. Changes in UI design.

--- zaza_interior/web/forms.py
+++ zaza_interior/web/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
@@ -14,9 +15,13 @@ class ContactFormCreate(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             if field_name == 'phone_number':
-                field.widget.attrs['placeholder'] = 'e.g. +359888123456'
+                field.widget.attrs['placeholder'] = _('e.g. +359XXXXXXXXX')
+                field.label = False
+            elif field_name == 'recaptcha':
+                continue
             else:
-                field.widget.attrs['placeholder'] = f"Enter your {field_name.replace('_', ' ')}"
-
+                verbose_name = self.Meta.model._meta.get_field(field_name).verbose_name
+                field.widget.attrs["placeholder"] = verbose_name
+                field.label = False
 
     recaptcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
